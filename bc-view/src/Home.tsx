b1db808412fccb92fd5488c901e655bb05af4472
Mike Holdsworth <mike@monowai.com>
2020-03-19 07:46:42 +0800

    chore: KC tidyup. Turn down noisy postres logging

--- bc-view/src/Home.tsx
+++ bc-view/src/Home.tsx
@@ -1,23 +1,17 @@
 import React from "react";
-import { withKeycloak } from "@react-keycloak/razzle";
-import { Landed } from "./Landed";
-import { KeycloakInstance } from "keycloak-js";
+import { useKeycloak } from "@react-keycloak/razzle";
+import { LoginRedirect } from "./common/auth/Login";
+import { Portfolios } from "./portfolio/Portfolios";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-//const Home = (props: WithTranslation & RouteComponentProps): JSX.Element => {
-class Home extends React.Component<{
-  keycloak: KeycloakInstance;
-  keycloakInitialized: boolean;
-  isServer: boolean;
-}> {
-  render(): JSX.Element {
-    const { keycloak } = this.props;
-    // const [keycloak] = useKeycloak();
-    if (keycloak) {
-      return <Landed />;
+const Home = (): JSX.Element => {
+  const [keycloak] = useKeycloak();
+  if (keycloak) {
+    if (!keycloak.token) {
+      return <LoginRedirect />;
     }
-    return <div>Initializing...</div>;
+    return <Portfolios />;
   }
-}
+  return <div>Initializing...</div>;
+};
 
-export default withKeycloak(Home);
+export default Home;

fedd05971fb1d4d52c8ffcb5bf10a4896f578f9c
Mike Holdsworth <mike@monowai.com>
2024-12-01 08:51:23 +0800

    perf: Cache resolved systemUser

--- svc-data/src/main/kotlin/com/beancounter/marketdata/registration/SystemUserRepository.kt
+++ svc-data/src/main/kotlin/com/beancounter/marketdata/registration/SystemUserRepository.kt
@@ -1,6 +1,7 @@
 package com.beancounter.marketdata.registration
 
 import com.beancounter.common.model.SystemUser
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.repository.CrudRepository
 import java.util.Optional
 
@@ -8,8 +9,7 @@ import java.util.Optional
  * CRUD repo for SystemUser.  A SystemUser can own portfolios in BC.
  */
 interface SystemUserRepository : CrudRepository<SystemUser, String?> {
-    fun findById(id: String): Optional<SystemUser>
-
+    @Cacheable("system.user")
     fun findByEmail(email: String): Optional<SystemUser>
 
     fun findByAuth0(auto0: String): Optional<SystemUser>

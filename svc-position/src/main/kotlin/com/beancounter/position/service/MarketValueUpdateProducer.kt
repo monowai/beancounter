f2132a1ef2e15455cf12314132502daeefe86e2b
Mike Holdsworth <mike@monowai.com>
2026-02-14 21:01:07 +0800

    devops: Don't panic if RMQ is down

--- svc-position/src/main/kotlin/com/beancounter/position/service/MarketValueUpdateProducer.kt
+++ svc-position/src/main/kotlin/com/beancounter/position/service/MarketValueUpdateProducer.kt
@@ -1,6 +1,7 @@
 package com.beancounter.position.service
 
 import com.beancounter.common.model.Portfolio
+import org.slf4j.LoggerFactory
 import org.springframework.cloud.stream.function.StreamBridge
 import org.springframework.kafka.support.KafkaHeaders
 import org.springframework.messaging.support.MessageBuilder
@@ -10,12 +11,18 @@ import org.springframework.stereotype.Service
 class MarketValueUpdateProducer(
     private val streamBridge: StreamBridge
 ) {
+    private val log = LoggerFactory.getLogger(MarketValueUpdateProducer::class.java)
+
     fun sendMessage(payload: Portfolio) {
-        val message =
-            MessageBuilder
-                .withPayload(payload)
-                .setHeader(KafkaHeaders.KEY, payload.id)
-                .build()
-        streamBridge.send("portfolioMarketValue-out-0", message)
+        try {
+            val message =
+                MessageBuilder
+                    .withPayload(payload)
+                    .setHeader(KafkaHeaders.KEY, payload.id)
+                    .build()
+            streamBridge.send("portfolioMarketValue-out-0", message)
+        } catch (e: Exception) {
+            log.error("Failed to send market value update for portfolio {}: {}", payload.code, e.message)
+        }
     }
 }
